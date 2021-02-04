9001e313fb7040aabe9e8e1f019372fcf6e7ec43
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-02-03 22:40:14 -0300

    comunicacion padre e hijo, interfase

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,5 @@
 import { Component} from '@angular/core';
-
-interface Personaje{
-  nombre:string;
-  poder:number;
-}
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
@@ -12,13 +8,45 @@ interface Personaje{
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre:'Goku ',
+      poder:1500
+    },
+    {
+      nombre:'Broly',
+      poder:1500
+    },
+    {
+      nombre:'Vegeta',
+      poder:1200
+    },
+    {
+      nombre:'Maestro roshi',
+      poder:777
+    },
+  ]
+
   nuevo:Personaje = {
-    nombre:'Trucks',
+    nombre:'',
     poder:1100
   }
 
   Agregar(){
+
+    //pregunta si no tiene nada el nuevo.nombre se sale del metodo
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+
+    this.personajes.push(this.nuevo);
     console.log(this.nuevo)
+
+    this.nuevo = {
+      nombre:'',
+      poder:0
+    }
+
   }
 
   
